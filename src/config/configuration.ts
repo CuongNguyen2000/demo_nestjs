1c753dd84abe20173b74c9425eb41cd9d830fe1c
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-03 08:50:48 +0700

    new commit

---
+++ src/config/configuration.ts
@@ -0,0 +1,7 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  keys: {
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
+    publicKey: process.env.PUBLIC_KEY.replace(/\\n/gm, '\n'),
+  },
+});
