8c95e67d7faead01dc195e52ad9f9cb39e57c316
CuongNguyen2000 <cn1122000@gmail.com>
2021-10-05 15:57:46 +0700

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/users.entity';
@@ -11,21 +11,17 @@ import { AuthModule } from './auth/auth.module';
             isGlobal: true,
             load: [configuration],
         }),
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                url: configService.get('database.uri'),
-                // host: configService.get('database.host'),
-                // port: configService.get('database.port'),
-                // user: configService.get('database.user'),
-                // password: configService.get('database.password'),
-                // name: configService.get('database.name'),
-                entities: [Users],
-                synchronize: true,
-            }),
-            inject: [ConfigService],
-        }),
+
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: 5432,
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE,
+            entities: [Users],
+            synchronize: true,
+          }),
         AuthModule,
     ],
     controllers: [],
